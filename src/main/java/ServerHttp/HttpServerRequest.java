cf6015da5b039ba16fc1a101ce0d405257b63001
Neir <neir.et.caetera@gmail.com>
2016-02-16 15:48:41 +0100

    indent + remove unused imports

--- src/main/java/ServerHttp/HttpServerRequest.java
+++ src/main/java/ServerHttp/HttpServerRequest.java
@@ -1,48 +1,42 @@
 package ServerHttp;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import request.Request;
 
 public class HttpServerRequest {
 	private Integer port;
 	private boolean started = false;
-	
+
 	private HttpServerRequest() {
 		this.port = 20000;
 	}
-	
+
 	private HttpServerRequest(Integer port) {
 		this.port = 20000;
-		
+
 		if (port > 10000 && port < 60000) {
 			this.port = port;
 		}
-		
+
 	}
-	
+
 	public void start() throws IOException {
 		if (started) {
 			System.err.println("Server already started");
 			return;
 		}
-		
+
 		Socket socket;
 		ServerSocket server = new ServerSocket(port);
-		
+
 		System.out.println("Server started at "+ port);
 		this.started = true;
 		while (true) {
 			try {
 				socket = server.accept();
-		        System.err.println("New client connected");
-		        new Thread( new Server(socket)).start();
+				System.err.println("New client connected");
+				new Thread( new Server(socket)).start();
 			} catch (IOException e1) {
 				e1.printStackTrace();
 				this.started = false;
@@ -50,7 +44,7 @@ public class HttpServerRequest {
 			}
 		}
 	} 
-	
+
 	public static void main(String[] args) throws IOException{
 		int port = 20000;		
 		HttpServerRequest server = new HttpServerRequest(port);
