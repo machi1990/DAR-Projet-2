3a509a6ab70a46404762b19c5bf4adbfaab06783
Neir <neir.et.caetera@gmail.com>
2016-02-16 15:16:11 +0100

    New parser by name

--- src/main/java/ServerHttp/HttpServerRequest.java
+++ src/main/java/ServerHttp/HttpServerRequest.java
@@ -52,52 +52,64 @@ public class HttpServerRequest {
 	        
 	        String s;
 	        String parser[];
+	        String stringRequest = "";
             try {
-            	int lineNb = 0;
+            	s = in.readLine();
+            	stringRequest += s + "\n";
+            	parser = s.split(" ");
+		    	request.setMethod(parser[0]);
+		    	//parser[1]; "/index" and parser[2]; "HTTP/1.1"
 				while ((s = in.readLine()) != null) {
 					
 					if (s.isEmpty()) {
 		                break;
 		            }
-					lineNb++;
 				    System.out.println(s);
-				    parser = s.split(" ");
-				    switch(lineNb){
-				    case 1 :
-				    	request.setMethod(parser[0]);
-				    	//parser[1]; "/index" and parser[2]; "HTTP/1.1"
-				    	break;
-				    case 2:
+				    parser = s.split(": ");
+				    switch(parser[0]){
+				    case "Host" :
 				    	request.setHost(parser[1]);
 				    	break;
-				    case 3:
+				    case "Connection" :
 				    	// Connection: keep-alive
 				    	break;
-				    case 4:
-				    	// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
-				    	break;
-				    case 5:
-				    	// Upgrade-Insecure-Requests: 1
-				    	break;
-				    case 6:
+				    case "User-Agent":
 				    	// User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36
 				    	break;
-				    case 7:
+				    case "Accept":
+				    	// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+				    	break;
+				    case "Upgrade-Insecure-Requests":
+				    	// Upgrade-Insecure-Requests: 1
+				    	break;
+				    case "Accept-Encoding":
 				    	// Accept-Encoding: gzip, deflate, sdch
 				    	break;
-				    case 8:
+				    case "Accept-Language":
 				    	// Accept-Language: fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4
 				    	break;
-				    case 9:
+				    case "Referer":
+				    	//Referer: http://localhost:20000/
 				    	break;
 				    default :
 				    }
 				}
+				
+		        out.write("HTTP/1.1 200 OK\r\n");
+		        out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
+		        out.write("Server: Apache/0.8.4\r\n");
+		        out.write("Content-Type: text/html\r\n");
+		        out.write("Content-Length: 57\r\n");
+		        out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n");
+		        out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n");
+		        out.write("\r\n");
+		        out.write("<TITLE>Exemple</TITLE>");
+		        out.write("<P>Ceci est votre requete :</P>");
+		        out.write("<H2>"+stringRequest+"</H2>");
 		        System.err.println("Connexion avec le client terminée");
 		        out.close();
 		        in.close();
 		        clientSocket.close();
-		    	lineNb = 0;
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
